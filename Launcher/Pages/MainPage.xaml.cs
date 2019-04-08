bf3353ddaa54bd074482a4c86ec9b0e84d1bfd5d
ToronLP <niklasanonym4@gmail.com>
2019-04-09 01:21:49 +0200

    Added Image "Slideshow"

--- Launcher/Pages/MainPage.xaml.cs
+++ Launcher/Pages/MainPage.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Net;
 using System.Threading;
+using System.Windows.Threading;
 
 namespace Launcher
 {
@@ -22,14 +23,57 @@ namespace Launcher
     /// </summary>
     public partial class MainPage : Page
     {
+        int imgIndex;
+
         public MainPage()
         {
             InitializeComponent();
+            //Slider.Source = new BitmapImage(new Uri(@"pack://application:,,,/Images/Slider/SKYR34BG.png"));
+            DispatcherTimerSample();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void downloadProgressBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+
+        public void DispatcherTimerSample()
+        {
+            InitializeComponent();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(5000);
+            timer.Tick += timer_Tick;
+            timer.Start();
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            if (imgIndex >= 3) {
+                imgIndex = 0;
+            }
+            switch (imgIndex) {
+                case 0:
+                    Slider.Opacity = 1;
+                    Slider2.Opacity = 0;
+                    Slider3.Opacity = 0;
+                    break;
+                case 1:
+                    Slider.Opacity = 0;
+                    Slider2.Opacity = 1;
+                    Slider3.Opacity = 0;
+                    break;
+                case 2:
+                    Slider.Opacity = 0;
+                    Slider2.Opacity = 0;
+                    Slider3.Opacity = 1;
+                    break;
+            }
+            imgIndex++;
         }
     }
 }
