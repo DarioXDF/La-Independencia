db218f8a4b21573b550bde1e730b92f3f915063d
Crus0e <dario.arzdorf@gmail.com>
2019-04-16 06:06:10 +0200

    Dowload Feature Update
    Pfade sind dumme Honks!!!!

--- Launcher/Pages/LoginPage.xaml.cs
+++ Launcher/Pages/LoginPage.xaml.cs
@@ -24,5 +24,10 @@ namespace Launcher
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
