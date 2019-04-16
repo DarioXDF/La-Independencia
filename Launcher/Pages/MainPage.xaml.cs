73f1cb9c6b9f3dea195acb19eaf2c757e2310c75
Crus0e <dario.arzdorf@gmail.com>
2019-04-16 04:23:47 +0200

    Download Feature

--- Launcher/Pages/MainPage.xaml.cs
+++ Launcher/Pages/MainPage.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Net;
 using System.Threading;
+using System.ComponentModel;
+using System.Windows.Threading;
 
 namespace Launcher
 {
@@ -22,6 +24,9 @@ namespace Launcher
     /// </summary>
     public partial class MainPage : Page
     {
+
+        private static BackgroundWorker backgroundWorker;
+
         public MainPage()
         {
             InitializeComponent();
@@ -29,7 +34,36 @@ namespace Launcher
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            /*Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            dlg.FileName = "myimage"; // Default file name
+            dlg.DefaultExt = ".zip"; // Default file extension
+            dlg.Filter = "Zip file (*.zip) | *.zip"; // Filter files by extension
 
+            // Show save file dialog box
+            Nullable<bool> result = dlg.ShowDialog();*/
+
+            // Process save file dialog box results
+            //if (result == true)
+            //{
+
+            //dlg.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            WebClient client = new WebClient();
+            client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
+            client.DownloadFileCompleted += new AsyncCompletedEventHandler(client_DownloadFileCompleted);
+            client.DownloadFileAsync(new Uri("https://github.com/DarioXDF/polygon-game-studio/archive/master.zip"), @"C:\Users\Crus0e\Desktop\LauncherDownload\master.zip");
+            // Save document
+            string filename = "master.zip";
+        }private void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            lblStatus.Content = "Complete!";
         }
+
+        private void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            downloadProgressBar.Maximum = 38530886;
+            lblStatus.Content = e.BytesReceived / 1000000 + "mb / " + 38530886/1000000 + "mb";
+            downloadProgressBar.Value = e.BytesReceived;
+        }
+
     }
 }
